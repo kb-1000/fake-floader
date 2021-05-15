a48fa6da4f5275292f0c1732b62ca4c9fed0c493
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-05-15 14:11:32 +0200

    Tune build system a bit

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,29 @@ dependencies {
     implementation("org.quiltmc:quilt-loader:0.13.1-rc.3")
     implementation("org.apache.logging.log4j:log4j-api:2.8.1")
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+}
+
+tasks.processResources {
+    filesMatching("fabric.mod.json") {
+        expand(
+            mutableMapOf(
+                "version" to project.version
+            )
+        )
+    }
+}
+
+tasks.jar {
+    from(project.file("LICENSE"))
+}
+
+tasks.getByName<Jar>("sourcesJar") {
+    from(project.file("LICENSE"))
+}
+
+// vim: et ts=4 sw=4
